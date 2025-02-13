3821d88a92c6544e265d5c7bd3f7fe0bfe3b827a
Keenan Wood <keenanw7@bu.edu>
2025-02-13 15:27:14 -0500

    Formula validation

--- ElastoPlastic/src/elastoplatic.py
+++ ElastoPlastic/src/elastoplatic.py
@@ -35,22 +35,25 @@ class ElastoPlastic:
  #       if len(input_set_strain) < 1: raise Exception("Array of strain increments should have at least one element")
  #       self._set_strain = input_set_strain
 
-    def stretch(self, set_strain: np.array, hard_iso: float, hard_kin):
+    def stretch(self, set_strain: np.array, hard_iso: float, hard_kin: float):
         # Validate inputs
         if len(set_strain) < 1: raise Exception("Array of strain increments should have at least one element")
-        if abs(hard_iso-.5) > .5: raise Exception("Isotropic hardening parameter must be between 0 (off) and 1 (on)")
-        if abs(hard_kin-.5) > .5: raise Exception("Kinematic hardening parameter must be between 0 (off) and 1 (on)")
+        if hard_iso < 0 or hard_iso > 1: raise Exception("Isotropic hardening parameter must be between 0 (off) and 1 (on)")
+        if hard_kin < 0 or hard_kin > 1: raise Exception("Kinematic hardening parameter must be between 0 (off) and 1 (on)")
         
         for strain_incr in set_strain:
-            # Update stress and strain (Predictor-Corrector in 1 step)
-            self.yield_stress = self.yield_stress + hard_iso*self.mat.H*self.strain
             self.strain = self.strain + strain_incr
 
+            # Update stress, back stress, and yield surface (Predictor-Corrector in 1 step using min())
             elastic_stress = self.stress + self.mat.E*strain_incr - self.back_stress
             phi = min(0, abs(elastic_stress) - self.yield_stress)
             plastic_strain = phi / (self.mat.E + self.mat.H)
             directed_strain = plastic_strain * np.sign(elastic_stress)
             self.stress = elastic_stress - self.mat.E * directed_strain
+
+            # Isotropic Part - Von-Mises Yield Criterion -> d(yield_stress) = 3/2 * h * d(plastic_strain)
+            self.yield_stress = self.yield_stress + hard_iso * self.mat.H * plastic_strain
+            # Kinematic Part - Prager's Rule -> d(back_stress) = c * d(plastic_strain)
             self.back_stress = self.back_stress + hard_kin * self.mat.H * directed_strain
 
             ## Compare with lecture notes - confusing update steps
