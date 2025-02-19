4e9af71200dd73b5c1600bea5363549bd116cd8c
Keenan Wood <keenanw7@bu.edu>
2025-02-19 14:26:58 -0500

    Updated tests

--- ElastoPlastic/src/elastoplastic.py
+++ ElastoPlastic/src/elastoplastic.py
@@ -24,16 +24,6 @@ class ElastoPlastic:
         self.stress = stress
         self.back_stress = back_stress
 
-    # Validate Instance Inputs
- #   @property
- #   def set_strain(self):
- #       return self._set_strain
-    
- #   @set_strain.setter
- #   def set_strain(self, input_set_strain):
- #       if len(input_set_strain) < 1: raise Exception("Array of strain increments should have at least one element")
- #       self._set_strain = input_set_strain
-
     def stretch(self, set_strain: np.array, hard_iso: float, hard_kin: float):
         # Validate inputs
         if set_strain.size < 1: raise Exception("Array of strain increments should have at least one element")
@@ -57,30 +47,3 @@ class ElastoPlastic:
             self.back_stress = self.back_stress + hard_kin * self.mat.H * signed_strain
 
             ## Compare with lecture notes - confusing update steps
-
-## Test/Debugging script
-if 1 == 0:
-    alum = material('aluminum', 70, 70/9, 0.095)
-    elasto_alum = ElastoPlastic(alum, 0, 0)
-    # strain_incr_array = np.concatenate((np.ones(100) * .002/100, -np.ones(100) * .002/100), axis=0)
-    #strain_incr_array = np.ones(100) * .002/100
-    strain_incr_array = np.array([.002])
-    N_steps = strain_incr_array.size
-    strain = np.zeros(N_steps + 1)
-    stress = np.zeros(N_steps + 1)
-    back_stress = np.zeros(N_steps + 1)
-    yield_stress = np.zeros(N_steps + 1)
-    strain[0] = elasto_alum.strain
-    stress[0] = elasto_alum.stress
-    back_stress[0] = elasto_alum.back_stress
-    yield_stress[0] = elasto_alum.yield_stress
-    for i_step in range(1, N_steps + 1):
-        elasto_alum.stretch(strain_incr_array[i_step-1], 1, 0)
-        strain[i_step] = elasto_alum.strain
-        stress[i_step] = elasto_alum.stress
-        back_stress[i_step] = elasto_alum.back_stress
-        yield_stress[i_step] = elasto_alum.yield_stress
-    plt.figure()
-    plt.plot(strain*10**3, stress, "-k", label="Stress")
-    plt.plot(strain*10**3, back_stress, ":b", label="Back Stress")
-    plt.plot(strain*10**3, yield_stress, "--r", label="Yield Stress")
--- ElastoPlastic/tests/test_elastoplastic.py
+++ ElastoPlastic/tests/test_elastoplastic.py
@@ -13,28 +13,28 @@ def test_isotropic_steel():
     test_steel = ElastoPlastic(steel, 0, 0)
     set_strain = np.array([.01, .01, .01, -.03, .05, -.08, .1, -.7])
     test_steel.stretch(set_strain, 1, 0)
-    assert true
+    assert True
 
 def test_kinematic_steel():
     test_steel = ElastoPlastic(steel, 0, 0)
     set_strain = np.array([.01, .01, .01, -.03, .05, -.08, .1, -.7])
     test_steel.stretch(set_strain, 1, 0)
-    assert true
+    assert True
 
 def test_isotropic_kinematic_steel():
     test_steel = ElastoPlastic(steel, 0, 0)
     set_strain = np.array([.01, .01, .01, -.03, .05, -.08, .1, -.7])
     test_steel.stretch(set_strain, 1, 1)
-    assert true
+    assert True
 
 def test_isotropic_nylon_prestrain():
     test_nylon = ElastoPlastic(nylon, .3, 0)
     set_strain = np.array([.01, .01, .01, -.03, .05, -.08, .1, -.7])
     test_nylon.stretch(set_strain, 1, 0)
-    assert true
+    assert True
 
 def test_kinematic_nylon_backstress():
     test_nylon = ElastoPlastic(nylon, .3, 100)
     set_strain = np.array([.01, .01, .01, -.03, .05, -.08, .1, -.7])
     test_nylon.stretch(set_strain, 0, 1)
-    assert true
+    assert True
